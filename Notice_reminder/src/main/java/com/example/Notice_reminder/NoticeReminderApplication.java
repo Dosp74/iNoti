429c1667696eaa03602181ddb0a6cbef96f06dfd
lsh8110 <lsklyj0118@naver.com>
2024-11-30 04:54:11 +0900

    크롤링 메일발송 수정

--- Notice_reminder/src/main/java/com/example/Notice_reminder/NoticeReminderApplication.java
+++ Notice_reminder/src/main/java/com/example/Notice_reminder/NoticeReminderApplication.java
@@ -1,26 +1,12 @@
 package com.example.Notice_reminder;
 
-import com.example.Notice_reminder.service.CrawlerService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class NoticeReminderApplication implements CommandLineRunner {
-
-	private final CrawlerService crawlerService;
-
-	@Autowired
-	public NoticeReminderApplication(CrawlerService crawlerService) {
-		this.crawlerService = crawlerService;
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		crawlerService.startCrawl();  // 크롤링 서비스 실행
-	}
-
+@EnableScheduling
+public class NoticeReminderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NoticeReminderApplication.class, args);
 	}
